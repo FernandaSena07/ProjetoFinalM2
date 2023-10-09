2e45f164bcc2cc49dfe5787136b68b16df2fcdea
Jessica <jb229507@gmail.com>
2023-10-09 19:39:36 -0300

    organizacaoPastas

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -23,22 +23,79 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecionar o botão "Limpar" pelo ID.
+ 
   const btnLimpar = document.getElementById('btnLimpar');
 
-  // Adicionar um ouvinte de evento de clique ao botão "Limpar".
+ 
   btnLimpar.addEventListener('click', function () {
-    // Selecionar os campos de texto e o campo de textarea pelo nome dos elementos.
+   
     const nomeInput = document.querySelector('input[name="username"]');
     const emailInput = document.querySelector('input[name="email"]');
     const telefoneInput = document.querySelector('input[name="phone"]');
     const mensagemTextarea = document.querySelector('textarea[name="message"]');
 
-    // Limpar os valores dos campos definindo-os como uma string vazia.
+   
     nomeInput.value = "";
     emailInput.value = "";
     telefoneInput.value = "";
     mensagemTextarea.value = "";
   });
 });
+
+
+const form = document.getElementById("form");
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const senha = document.getElementById("senha")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    checkForm();
+})
+
+function checkusername() {
+    const usernameValue = username.value;
+
+    if (usernameValue === "") {
+        errorInput(username, "É necessario colocar um nome")
+    } else {
+        const formItem = username.parentElement;
+        formItem.className = "container-form"
+    }
+
+
+}
+
+function checkEmail() {
+    const emailValue = email.value;
+
+    if (emailValue === "") {
+        errorInput(email, "É necessario preencher o email")
+    } else {
+        const formItem = email.parentElement;
+        formItem.className = "container-form"
+    }
+}
+
+function checkSenha() {
+    const senhaValue = senha.value;
+
+    if (senhaValue === "") {
+        errorInput(senha, "É necessario colocar uma senha")
+    } else {
+        const formItem = senha.parentElement;
+        formItem.className = "container-form"
+    }
+
+}
+
+
+function checkForm() {
+    checkusername();
+    checkEmail();
+    checkSenha();
+
+
+
 
